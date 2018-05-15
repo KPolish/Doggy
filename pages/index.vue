4a0b4694879bc1b2bb4c7b6b5713832af167a595
KPolish <com94ok@naver.com>
2018-05-15 10:46:22 +0900

    20180515_doguxt#02

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,17 @@
 <template>
-  <section class="container">
-    <div class="content">
-      <h1 class="title is-1">러버독</h1>
-      <h2 class="subtitle">Alphadog has a Megalomaniac</h2>
+  <section class="section">
+    <div class="hero is-danger">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-1">러버독</h1>
+          <h2 class="subtitle">Alphadog has a Megalomaniac</h2>
+        </div>
+      </div>
     </div>
+    <article class="message">
+      <div class="message-header">what is dog</div>
+      <div class="message-body">데꿀데굴멍멍왈왈</div>
+    </article>
   </section>
 </template>
 
