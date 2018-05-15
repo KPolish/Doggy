91859a4d224d98b2d01ed1db1215e2a1176edaf0
KPolish <com94ok@naver.com>
2018-05-15 10:09:08 +0900

    20180515_doguxt#01

---
+++ commit_push.bat
@@ -0,0 +1,3 @@
+git add --all
+git commit -m "20180515_doguxt#01"
+git push -u origin master
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="container">
+    <div class="content">
+      <h1 class="title is-1">러버독</h1>
+      <h2 class="subtitle">Alphadog has a Megalomaniac</h2>
+    </div>
+  </section>
+</template>
+
+<script>
+</script>
