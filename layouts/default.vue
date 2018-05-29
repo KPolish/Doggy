28b248035af293e07c7ffe98f8dc6642a58602a8
KPolish <com94ok@naver.com>
2018-05-29 12:52:28 +0900

    20180529_doguxt#09

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
+    <nav class="navbar">
+      <div class="navbar-brand">
+        <nuxt-link to="/" class="navbar-item">러버독</nuxt-link>
+      </div>
+      <div class="navbar-menu is-active">
+        <div class="navbar-end">
+          <nuxt-link to="/about" class="navbar-item">소개</nuxt-link>
+        </div>
+      </div>
+    </nav>
     <nuxt/>
+  <footer class="footer has-text-centered">
+    <P>2018's 목원대학교 러버독. 러버독 오올 롸잍트 리-자브드</P>    
+  </footer>
   </div>
 </template>
---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+    <section class="section">
+        <article class="hero is-info">
+            <div class="hero-body">
+                    <h1 class="title">러버-독 인트로듀스</h1>
+            </div>
+        </article>
+    </section>
+</template>
