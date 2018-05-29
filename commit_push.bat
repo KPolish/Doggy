24e868f3206f3f736474f3e9027d2775e7972d4a
KPolish <com94ok@naver.com>
2018-05-29 11:10:26 +0900

    20180529_doguxt#03

--- commit_push.bat
+++ commit_push.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "20180529_doguxt#02"
+git commit -m "20180529_doguxt#03"
 git push -u origin master
--- pages/dogs/_dog.vue
+++ pages/dogs/_dog.vue
@@ -1,5 +1,22 @@
 <template>
     <section class="section">
-        
+        <article class="hero">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">저는 {{dogName}} 입니다.</h1>
+                </div>
+            </div>
+        </article>
+
+        {{dogName}}
     </section>
 </template>
+<script>
+    export default {
+        asyncData({params}) {
+            return{
+                dogName: params.dog
+            };
+        }
+    }
+</script>
