184ca4dc2bc6324831ad435fba842fb27d32e4da
KPolish <com94ok@naver.com>
2018-05-29 11:34:41 +0900

    20180529_doguxt#04

--- commit_push.bat
+++ commit_push.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "20180529_doguxt#03"
+git commit -m "20180529_doguxt#04"
 git push -u origin master
--- pages/dogs/_dog.vue
+++ pages/dogs/_dog.vue
@@ -4,6 +4,7 @@
             <div class="hero-body">
                 <div class="container">
                     <h1 class="title">저는 {{dogName}} 입니다.</h1>
+                    <h2 class="subtitle">그러합니다.</h2>
                 </div>
             </div>
         </article>
